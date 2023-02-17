301a49becd23e3e2fe97befa39245bdcb6992769
raton <verardi.gino@gmail.com>
2023-02-17 15:38:48 -0300

    se añadio la bd prostgresql

--- bikedata_project/bikedata_project/settings.py
+++ bikedata_project/bikedata_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'bikedata_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'info_bikes',
+        'USER': 'biker',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
